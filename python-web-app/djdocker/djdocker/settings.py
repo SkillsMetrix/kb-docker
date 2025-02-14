492b2b33ad36c3ff85a62b48d44b7d93aa02967d
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2025-02-14 12:25:34 +0530

    Update settings.py

--- python-web-app/djdocker/djdocker/settings.py
+++ python-web-app/djdocker/djdocker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z^5=(ju+71s&$1qd-j#w!(_ewemxb++cmxsc#1p^#w9^=zbxmd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
