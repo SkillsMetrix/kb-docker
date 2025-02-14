a65a39669c9647e9e701b53e93660d9bd8c8837f
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2025-02-14 14:28:53 +0530

    Update settings.py

--- django-docker-mysql/dedupe/settings.py
+++ django-docker-mysql/dedupe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kq6ln5@f68j#sf0cp)5^mr1*+c4qba)m36e-m^&ybu6+vv!t$l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
